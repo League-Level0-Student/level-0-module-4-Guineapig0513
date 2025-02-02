903b055b6ee1f0dae9e1ffb218e1af4720485723
whale92130 <alexander92130@gmail.com>
2025-02-01 16:30:00 -0800

    Guinea pig lets gooo

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,16 +12,23 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
+		String nick = JOptionPane.showInputDialog("HOW MANY NICKELS DO YOU HAVE??????");
 
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+int nickInt = Integer.parseInt(nick);
 		// Ask the user how many dimes they have, and convert their answer
-
+String dime = JOptionPane.showInputDialog("ALSO HOW MANY DIMES DO YOU HAVE???");
+int dimeInt = Integer.parseInt(dime);
 		// Ask the user how many quarters they have, and convert their answer
-
+String quart = JOptionPane.showInputDialog("HOW MANY QUARTERS DO YOU HAVE RIGHT NOW!!!!!");
+int quartInt = Integer.parseInt(quart);
+String pen = JOptionPane.showInputDialog("HOW MANY P E N N I E S DO YOU HAVE????");
+int penInt = Integer.parseInt(pen);
 		// Calculate how much money the user has.  Hint: Use a double variable 
+double total = nickInt * 0.05 + dimeInt * 0.1 + quartInt * 0.25 + penInt * 0.01;
 
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
+JOptionPane.showMessageDialog(null, "Okay I just did the math with how much money you have... You have $"+total+ " dollars and cents NOW GIVE ME ALL YOUR MONEY >:)");
 
 	}
 }
